c06dafa0290ee17047516babb3a0229e70e08d2b
Torsten Uhlmann <tuhlmann@agynamix.de>
2026-01-08 16:33:06 +0100

    Add invoicer promo banner to index page

--- src/blocks/TextLink.tsx
+++ src/blocks/TextLink.tsx
@@ -4,6 +4,8 @@ import classNames from "classnames"
 interface Props {
   headerColor?: string
   to: string
+  target?: string
+  rel?: string
   className?: string
   children: any
 }
@@ -11,7 +13,12 @@ interface Props {
 export function TextLink(props: Props) {
   // transition ease-in delay-500
   return (
-    <a class={classNames("text-link no-underline w-fit", props.className)} href={props.to}>
+    <a
+      class={classNames("text-link no-underline w-fit", props.className)}
+      href={props.to}
+      target={props.target}
+      rel={props.rel}
+    >
       {props.children}
     </a>
   )
